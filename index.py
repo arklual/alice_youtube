e09dec2e709d18aa690309e480745f8929ab2f24
arklual <arklual1@gmail.com>
2023-08-16 13:49:10 +0300

    update

--- index.py
+++ index.py
@@ -29,7 +29,10 @@ async def handler(request: Request):
             ids = json.load(f)
         if ids == []:
             return alice_api.get_response('Ещё гружу', event)
-        return alice_api.get_response('Начинаю воспроизведение', event, tts=f"<speaker audio='dialogs-upload/46f17381-7ce6-4393-a3cb-d989a6e8d906/{ids[0]}.opus'>")   
+        mmsg = ''
+        for id in ids:
+            mmsg += f"<speaker audio='dialogs-upload/46f17381-7ce6-4393-a3cb-d989a6e8d906/{id}.opus'>"
+        return alice_api.get_response('Начинаю воспроизведение', event, tts=mmsg)   
     elif search and r and 'далее' in search:
         num = int(search.split(' ')[1].replace('один', '1').replace('два', '2').replace('три', '3').replace('четыре', '4').replace('пять', '5').replace('шесть', '6').replace('семь', '7').replace('восемь', '8').replace('девять', '9'))-1
         with open('ids.json', 'r') as f:
